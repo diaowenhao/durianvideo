f5143a0256c9c36be24b85b580a5268619be60cf
yue497483339 <yue315yun>
2017-02-20 09:43:42 +0800

    test1

---
+++ app/src/main/java/qf/com/durianvideo/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package qf.com.durianvideo.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import qf.com.durianvideo.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
