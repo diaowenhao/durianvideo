880cf905bb63c3b14ca6733fe00782ef97a5f695
yue497483339 <yue315yun>
2017-02-20 18:45:02 +0800

    更新3

--- app/src/main/java/qf/com/durianvideo/fragment/HomePageFragment.java
+++ app/src/main/java/qf/com/durianvideo/fragment/HomePageFragment.java
@@ -1,26 +1,32 @@
 package qf.com.durianvideo.fragment;
 
 
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import qf.com.durianvideo.R;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class DiscoverFragment extends Fragment {
+public class HomePageFragment extends Fragment {
+
+
+    public HomePageFragment() {
+        // Required empty public constructor
+    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_discover, container, false);
+        View view = inflater.inflate(R.layout.fragment_home_page, container, false);
 
+        return view;
     }
 
 }
-
