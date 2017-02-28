fb1194e8bb48a168db5d82246c7f64590d940cd8
hanbinxaiopenyou <hanbinxiaopenyou@163.com>
2017-02-28 11:58:16 +0800

    28号

--- app/src/main/java/qf/com/durianvideo/adapter/MyAdapterofHomePage.java
+++ app/src/main/java/qf/com/durianvideo/adapter/MyAdapterofHomePage.java
@@ -1,6 +1,7 @@
 package qf.com.durianvideo.adapter;
 
 import android.content.Context;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,21 +18,56 @@ import qf.com.durianvideo.R;
  * Created by lenovo on 2017/2/27.
  */
 
-public class MyAdapterofHomePage extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
+public class MyAdapterofHomePage extends BaseRecyclerAdapter<String>{
     Context mContext;
     List<String> datas=new ArrayList<String>();
+
+    InnerAdapterofHomePage mInnerAdapterofHomePage;
+
     public MyAdapterofHomePage( Context mContext, List<String> datas){
+        super(mContext);
         this.mContext = mContext;
         this.datas = datas;
     }
 
+    @Override
+    public RecyclerView.ViewHolder onCreate(ViewGroup parent, int viewType) {
+        View layout = LayoutInflater.from(mContext).inflate(R.layout.homepage_normal_item, parent, false);
+        //加载item并且返回
+        return new MyHolder(layout);
+    }
+
+    @Override
+    public void onBind(RecyclerView.ViewHolder viewHolder, int RealPosition, String data) {
+        if(viewHolder instanceof MyHolder) {
+            ((MyHolder) viewHolder).text.setText(datas.get(RealPosition));
+        }
+    }
+
+    class MyHolder extends BaseRecyclerAdapter.Holder {
+        TextView text;
+        RecyclerView mRecyclerView;
+        public MyHolder(View itemView) {
+        super(itemView);
+        text = (TextView) itemView.findViewById(R.id.text1);
+        mRecyclerView = (RecyclerView) itemView.findViewById(R.id.homepage_recycler);
+        initData();
+            //设置成列表显示的
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(mContext));
+            //设置适配器---还没有整完
+            mInnerAdapterofHomePage = new InnerAdapterofHomePage(mContext);
+        mRecyclerView.setAdapter(mInnerAdapterofHomePage);
+    }
+
+        public void initData(){}
+}
     /**
      * 创建ViewHolder这个模板的
      * @param parent
      * @param viewType ——这个参数是实现多头部的关键
      * @return
      */
-    @Override
+   /* @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view =LayoutInflater.from(mContext).inflate(R.layout.homepage_normal_item,null);
         ViewHolder viewHolder = new ViewHolder(view);
@@ -49,10 +85,11 @@ public class MyAdapterofHomePage extends RecyclerView.Adapter<RecyclerView.ViewH
         return datas.size();
     }
 
-    /**
+
+    *//**
      * 模板类  ----> ListView中的那个模板类
      * 还要用来找控件
-     */
+     *//*
     public class ViewHolder extends RecyclerView.ViewHolder{
         TextView mTextView1;
         TextView mTextView2;
@@ -78,5 +115,5 @@ public class MyAdapterofHomePage extends RecyclerView.Adapter<RecyclerView.ViewH
         public ImageView getmImageView() {
             return mImageView;
         }
-    }
+    }*/
 }
